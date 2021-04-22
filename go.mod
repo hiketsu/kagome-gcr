2b81309e263b235f60034e8cee64cd6bc5786e6e
Jeff <jeff@hiketsu.info>
2021-04-22 21:15:25 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module kagome-gcr
+
+go 1.15
+
+require (
+	github.com/ikawaha/kagome-dict/ipa v1.0.2
+	github.com/ikawaha/kagome/v2 v2.4.4
+	github.com/rs/cors v1.7.0
+)
---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	// "github.com/ikawaha/kagome-dict/dict"
+	"github.com/ikawaha/kagome-dict/ipa"
+	// "github.com/ikawaha/kagome-dict/uni"
+	"github.com/ikawaha/kagome/v2/tokenizer"
+	"github.com/rs/cors"
+)
+
+type TokenizeHandler struct {
+	tokenizer *tokenizer.Tokenizer
+}
+
+func (h *TokenizeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type record struct {
+		ID            int      `json:"id"`
+		Start         int      `json:"start"`
+		End           int      `json:"end"`
+		Surface       string   `json:"surface"`
+		Class         string   `json:"class"`
+		POS           []string `json:"pos"`
+		BaseForm      string   `json:"base_form"`
+		Reading       string   `json:"reading"`
+		Pronunciation string   `json:"pronunciation"`
+		Features      []string `json:"features"`
+	}
+
+	var body struct {
+		Input string `json:"sentence"`
+		Mode  string `json:"mode,omitempty"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := fmt.Sprintf("{\"status\":false,\"error\":\"%v\"}\n", err)
+		if _, err := fmt.Fprint(w, msg); err != nil {
+			log.Printf("write error, %s, %v", msg, err)
+		}
+		return
+	}
+	if body.Input == "" {
+		msg := "{\"status\":true,\"tokens\":[]}\n"
+		if _, err := fmt.Fprint(w, msg); err != nil {
+			log.Fatalf("write error, %s, %v", msg, err)
+		}
+		return
+	}
+	mode := tokenizer.Normal
+	switch body.Mode {
+	case "Search":
+		mode = tokenizer.Search
+	case "Extended":
+		mode = tokenizer.Extended
+	}
+	tokens := h.tokenizer.Analyze(body.Input, mode)
+	var rsp []record
+	for _, tok := range tokens {
+		if tok.ID == tokenizer.BosEosID {
+			continue
+		}
+		m := record{
+			ID:       tok.ID,
+			Start:    tok.Start,
+			End:      tok.End,
+			Surface:  tok.Surface,
+			Class:    fmt.Sprintf("%v", tok.Class),
+			POS:      tok.POS(),
+			Features: tok.Features(),
+		}
+		m.BaseForm, _ = tok.BaseForm()
+		m.Reading, _ = tok.Reading()
+		m.Pronunciation, _ = tok.Pronunciation()
+		rsp = append(rsp, m)
+	}
+	j, err := json.Marshal(struct {
+		Status bool     `json:"status"`
+		Tokens []record `json:"tokens"`
+	}{
+		Status: true,
+		Tokens: rsp,
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		msg := fmt.Sprintf("{\"status\":false,\"error\":\"%v\"}", err)
+		if _, err := fmt.Fprint(w, msg); err != nil {
+			log.Printf("write error, %s, %v", msg, err)
+		}
+		return
+	}
+	if _, err := w.Write(j); err != nil {
+		log.Printf("write response json error, %v, %+v", err, body.Input)
+		return
+	}
+}
+
+func main() {
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.Nop())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/tokenize", &TokenizeHandler{tokenizer: t})
+
+	handler := cors.Default().Handler(mux)
+
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
